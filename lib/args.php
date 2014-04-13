00cf7e76ad1b48ae96f1df82a04385d7ad6c6798
s-akiyoshi <akiyoshi.satoshi@lancers.co.jp>
2014-04-13 17:27:14 +0900

    通常のメッセージも送信できるように修正

--- lib/args.php
+++ lib/args.php
@@ -20,6 +20,10 @@ function get_args($argc, $argv) {
     // 期限はデフォルト当日
     $args['limit'] = empty($args['limit']) ? time() : strtotime($args['limit']);
 
+    // typeはデフォルトmessages
+    $args['type'] = empty($args['type']) ? 'messages' : $args['type'];
+    check_type($args['type']);
+
     return $args;
 }
 
@@ -36,16 +40,24 @@ function get_args($argc, $argv) {
 function check_args($args, $requiredArgs = array('token')) {
     $success = true;
     if (empty($args)) $success = false;
-    var_export($args);
 
     // 必要パラメータ確認
     foreach ($requiredArgs as $key) {
-        var_dump($key);
         if (empty($args[$key])) $success = false;
     }
     if ($success == false) {
+        var_export($args);
         echo "引数が不正なため処理を停止しました\n";
         echo "ex) php put_task.php token==TOKEN room_id==ROOM_ID to_ids==ID_1,ID_2,ID_3 limit=Y-m-d body==本文\n";
         exit;
     }
+}
+
+function check_type($type) {
+    if (!in_array($type, array('messages', 'tasks'))) {
+        echo "引数が不正なため処理を停止しました\n";
+        echo "type は messages か tasks のみ可能です。\n";
+        exit;
+    }
+    return true;
 }
--- push.php
+++ push.php
@@ -1,20 +1,25 @@
 <?php
 
-require_once('./post.php');
-require_once('./args.php');
-require_once('./body.php');
+require_once('lib/post.php');
+require_once('lib/args.php');
+require_once('lib/body.php');
 
 // 引数のパラメータを取得
 $args = get_args($argc, $argv);
-check_args($args, array('body', 'token', 'room_id', 'to_ids'));
+check_args($args, array('body', 'token', 'room_id'));
 
-// Toをセット
-$args['body'] = set_to_message($args['body'], $args['to_ids']);
+if ($args['type'] == 'task') check_args($args, array('to_ids'));
+
 
 $params = array(
     'body'   => $args['body'],
     'limit'  => $args['limit'],
-    'to_ids' => $args['to_ids'],
 );
 
-post($args['room_id'], $args['token'], $params, 'tasks');
+if (!empty($args['to_ids'])) {
+    $params['to_ids'] = $args['to_ids'];
+    // to_ids 設定がある場合は、メッセージに[To:]をつける
+    $params['body'] = set_to_message($args['body'], $args['to_ids']);
+}
+
+post($args['room_id'], $args['token'], $params, $args['type']);
